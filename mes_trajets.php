7c13607b67ee4ad40f476a7563f3e090c0aefd56
Ysaeka <binou75@gmail.com>
2025-07-18 16:08:09 +0200

    modification de la page mes trajets, retrait de l'heure

--- mes_trajets.php
+++ mes_trajets.php
@@ -40,31 +40,29 @@ try {
                                 $heure = new DateTime($trajet['heure_depart']);
                             ?>
                             <span class ="date"> Date : <?= $date->format('d/m/Y')?></span>
-                            <span class ="horaires"> <?= $heure->format('H\hi') ?></span>
                             <span class ="lieu depart_arrivee"> <?= ($trajet['lieu_depart'])?> ---> <?=($trajet ['lieu_arrivee'])?></span>
                             <span class ="prixTrajet"><?= ($trajet['prix_personne']) ?> € </span>
-                        </span>
-                        
-                        <?php if ($trajet['statut'] !== 'terminé') : ?>
-                            <form method="POST" action="statut_covoiturage.php" class="formStatut">
-                                <input type="hidden" name="trajet_id" value="<?= $trajet['covoiturage_id'] ?>">
+                            <?php if ($trajet['statut'] !== 'terminé') : ?>
+                                <form method="POST" action="statut_covoiturage.php" class="formStatut">
+                                    <input type="hidden" name="trajet_id" value="<?= $trajet['covoiturage_id'] ?>">
 
-                                <?php
-                                    $prochaine_action = 'demarrer';
-                                    if ($trajet['statut'] === 'en_cours'){
-                                        $prochaine_action = "terminer";
-                                    }
-                                ?>
-                                <input type="hidden" name ="action" value="<?= $prochaine_action ?>">
-                                <label class="switch">
-                                    <input type ="checkbox" onchange="this.form.submit()" <?=$trajet['statut'] === 'en_cours' ? 'checked' : '' ?>>
-                                    <span class="slider"></span>
-                                </label>
-                                    <span><?= $prochaine_action === 'demarrer' ? 'Démarrer' : 'Terminer'?></span>
-                            </form>
-                        <?php else: ?>
+                                    <?php
+                                        $prochaine_action = 'demarrer';
+                                        if ($trajet['statut'] === 'en_cours'){
+                                            $prochaine_action = "terminer";
+                                        }
+                                    ?>
+                                    <input type="hidden" name ="action" value="<?= $prochaine_action ?>">
+                                    <label class="switch">
+                                        <input type ="checkbox" onchange="this.form.submit()" <?=$trajet['statut'] === 'en_cours' ? 'checked' : '' ?>>
+                                        <span class="slider"></span>
+                                    </label>
+                                        <span><?= $prochaine_action === 'demarrer' ? 'Démarrer le covoiturage' : 'Arrivée à destination'?></span>
+                                </form>
+                            <?php else: ?>
                             <span class="statutTermine"> ✅ Trajet terminé </span>
-                        <?php endif; ?>
+                            <?php endif; ?>
+                        </span>
                     </div>
                 <?php endforeach ?>
             <?php endif; ?>

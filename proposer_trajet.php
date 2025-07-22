473a825058d072e48355526bd85ad2f7d8ce66a5
Ysaeka <binou75@gmail.com>
2025-07-22 11:57:17 +0200

    modif css proposer un trajet

--- proposer_trajet.php
+++ proposer_trajet.php
@@ -18,9 +18,12 @@
             <div class = "imgSlogan">
                 <p>Solution économique pour les voyageurs soucieux de l’environnement.</p>
             </div>
-            <h2> Pour proposer un trajet nous vous invitons à vous connecter :</h2>
-                <a href = "connexion.php"> Connectez-vous ici ! </a>
-                <a href = "inscription.php"> Vous n'avez pas de compte ? </a>
+            <h2> Nous vous invitons à vous connecter à votre espace pour proposer un trajet :</h2>
+            <div class ="linkVisiteur">
+                    <a class="visiteurConn" href = "connexion.php"> Connectez-vous ici ! </a>
+                    <br>
+                    <a href = "inscription.php"> Vous n'avez pas de compte ? </a>
+            </div>
         </section>
 
     <?php else : ?>
@@ -35,31 +38,31 @@
             <h2> Proposer un trajet </h2>
             <form method ="POST" action="enregistrer_trajet.php" id="formTrajet" novalidate>
 
-                <label for="ville_depart"> Ville de départ :  </label>
+                <label for="ville_depart"> Ville de départ* :  </label>
                 <input type="text" name="ville_depart" id="ville_depart" placeholder="Entrez la ville de départ"/>
 
-                <label for="Ville_arrivee"> Ville de d'arrivée :  </label>
+                <label for="Ville_arrivee"> Ville de d'arrivée* :  </label>
                 <input type="text" name="ville_arrivee" id="ville_arrivee" placeholder="Entrez la ville d'arrivée"/>
 
-                <label for="date"> Date :  </label>
+                <label for="date"> Date* :  </label>
                 <input type="date" name="date_depart" id="date" placeholder="Date de départ" required/>
 
-                <label for="heure_depart"> Heure de départ :  </label>
+                <label for="heure_depart"> Heure de départ* :  </label>
                 <input type="time" name="heure_depart" id="heure_depart"/>
 
-                <label for="places"> Nombre de places disponibles (1-7) : </label>
+                <label for="places"> Nombre de places disponibles (1-7)* : </label>
                 <input type="number" name="places" id="places" min="1" max="7" required>
 
-                <label for="prix"> Prix en €/ personne :  </label>
+                <label for="prix"> Prix en €/ personne* :  </label>
                 <input type="number" name="prix" id="prix" />
 
-                <label for="VoyageEco"> Voyage écologique (Vehicule 100% electrique) : </label>
+                <label for="VoyageEco"> Voyage écologique (Vehicule 100% electrique)* : </label>
                 <input type="checkbox" name="VoyageEco" id="voyageEco" />
 
-                <label for="details"> Details du trajet :  </label>
+                <label for="details"> Details du trajet* :  </label>
                 <textarea name="details" id="details" placeholder="Lieu de rendez-vous..." required></textarea>
 
-                <label for="voiture_id"> Choisir un vehicule </label>
+                <label for="voiture_id"> Choisir un vehicule* </label>
                 <select name= "voiture_id" id="voiture_id" required>
                     <?php 
                         $userId = $_SESSION['users_id'];
@@ -79,18 +82,16 @@
                 </select>
 
                 <button type="submit" id="valider"> SOUMETTRE </button>
+                <div id="champs"><p>*Champs obligatoires</P></div>
             </form>
-            <div id="champs"><p>*Champs obligatoires</P></div>
         </div>
-
     <?php endif; ?>
-
 
     <script src="asset/JS/btn_login.js"></script>
 
-    <?php
-    require_once 'templates/footer.html'
-    ?>
+<?php
+require_once 'templates/footer.html'
+?>
 
 </body>
 </html>
